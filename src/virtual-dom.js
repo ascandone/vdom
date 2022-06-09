8228b98cd4d28990cd596286dea2364531ebaadc
Alessandro Scandone <alescandone@gmail.com>
2022-06-09 21:41:57 +0200

    refactored

--- src/virtual-dom.js
+++ src/virtual-dom.js
@@ -5,36 +5,35 @@ export class Vdom {
     this._root = node;
   }
 
+  _createNode(nodeParent, newVdom) {
+    if (typeof newVdom === "string") {
+      // text node
+      const strNode = document.createTextNode(newVdom);
+
+      // ##PATCH
+      nodeParent.appendChild(strNode);
+    } else {
+      // regular node
+      const node = document.createElement(newVdom.nodeName);
+
+      for (const propName in newVdom.props) {
+        // ##PATCH
+        node[propName] = newVdom.props[propName];
+      }
+
+      for (let newIndex = 0; newIndex < newVdom.children.length; newIndex++) {
+        this._renderRec(node, newIndex, undefined, newVdom.children[newIndex]);
+      }
+
+      // ##PATCH
+      nodeParent.appendChild(node);
+    }
+  }
+
   _renderRec(nodeParent, index, lastVdom, newVdom) {
     // Creating new node
     if (lastVdom === undefined && newVdom !== undefined) {
-      if (typeof newVdom === "string") {
-        // text node
-        const strNode = document.createTextNode(newVdom);
-
-        // ##PATCH
-        nodeParent.appendChild(strNode);
-      } else {
-        // regular node
-        const node = document.createElement(newVdom.nodeName);
-
-        for (const propName in newVdom.props) {
-          // ##PATCH
-          node[propName] = newVdom.props[propName];
-        }
-
-        for (let newIndex = 0; newIndex < newVdom.children.length; newIndex++) {
-          this._renderRec(
-            node,
-            newIndex,
-            undefined,
-            newVdom.children[newIndex]
-          );
-        }
-
-        // ##PATCH
-        nodeParent.appendChild(node);
-      }
+      this._createNode(nodeParent, newVdom);
     }
   }
 
