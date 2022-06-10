e16c27189e2e9f65ddccf13102690f66c280d8dd
ascandone <alescandone@gmail.com>
2022-06-10 14:09:11 +0200

    refactored

--- src/virtual-dom.js
+++ src/virtual-dom.js
@@ -8,6 +8,8 @@ import {
 const lastVdoms = new WeakMap();
 
 export class Vdom {
+  _patches = [];
+
   constructor(node) {
     this._root = node;
   }
@@ -17,13 +19,13 @@ export class Vdom {
     const node = nodeParent.childNodes[index];
     for (const propName in newVdom.props) {
       if (lastVdom.props[propName] !== newVdom.props[propName]) {
-        new SetProp(node, propName, newVdom.props[propName]).apply();
+        this._emit(new SetProp(node, propName, newVdom.props[propName]));
       }
     }
 
     for (const propName in lastVdom.props) {
       if (!(propName in newVdom.props)) {
-        new DeleteProp(node, propName).apply();
+        this._emit(new DeleteProp(node, propName));
       }
     }
 
@@ -33,7 +35,7 @@ export class Vdom {
     );
 
     for (let subIndex = longestIndex; subIndex >= 0; subIndex--) {
-      this._renderRec(
+      this._patch(
         node,
         subIndex,
         lastVdom.children[subIndex],
@@ -42,31 +44,45 @@ export class Vdom {
     }
   }
 
-  _renderRec(nodeParent, index, lastVdom, newVdom) {
+  _emit(patch) {
+    this._patches.push(patch);
+  }
+
+  _patch(nodeParent, index, lastVdom, newVdom) {
     if (lastVdom === newVdom) {
       return;
     }
 
     if (lastVdom === undefined && newVdom !== undefined) {
-      new AppendChild(nodeParent, newVdom).apply();
+      this._emit(new AppendChild(nodeParent, newVdom));
     } else if (lastVdom !== undefined && newVdom === undefined) {
-      new RemoveChild(nodeParent, index, lastVdom.props.ondelete).apply();
+      this._emit(new RemoveChild(nodeParent, index, lastVdom.props.ondelete));
     } else if (lastVdom !== undefined && newVdom !== undefined) {
       if (
         typeof newVdom === "string" ||
         typeof lastVdom === "string" ||
         lastVdom.nodeName !== newVdom.nodeName
       ) {
-        new ReplaceWith(nodeParent, index, newVdom).apply();
+        this._emit(new ReplaceWith(nodeParent, index, newVdom));
       } else {
         this._renderDiff(nodeParent, index, lastVdom, newVdom);
       }
     }
   }
 
-  render(newVdom) {
+  patch(newVdom) {
     const lastVdom = lastVdoms.get(this._root);
-    this._renderRec(this._root, 0, lastVdom, newVdom);
+    this._patch(this._root, 0, lastVdom, newVdom);
+    const ret = this._patches;
+    this._patches = [];
+    return ret;
+  }
+
+  render(newVdom) {
+    const patches = this.patch(newVdom);
+    for (const patch of patches) {
+      patch.apply();
+    }
     lastVdoms.set(this._root, newVdom);
   }
 }
